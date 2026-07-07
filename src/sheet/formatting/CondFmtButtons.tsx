78bf410cbea3c7df40ca4894b6dfc7439c1642f8
유용태 <teo.v@navercorp.com>
2026-07-08 04:54:18 +0900

    Align conditional formatting toolbar tooltips

--- src/sheet/formatting/CondFmtButtons.tsx
+++ src/sheet/formatting/CondFmtButtons.tsx
@@ -13,7 +13,7 @@ export function CondFmtButtons({ col, addCondRule, clearCondRules, ask }: Props)
   }
   return (
     <>
-      <button type="button" onClick={onAdd} title="조건부 서식" aria-label="조건부 서식 추가">🎨조건</button>
+      <button type="button" onClick={onAdd} title="조건부 서식 추가" aria-label="조건부 서식 추가">🎨조건</button>
       <button type="button" onClick={clearCondRules} title="조건부 서식 모두 해제" aria-label="조건부 서식 모두 해제">✕조건</button>
     </>
   )
